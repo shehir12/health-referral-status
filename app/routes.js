7f304a77d081d84dfc8b0dd56261b8896db95f5e
Leah Smith <leah.smith1@engineering.digital.dwp.gov.uk>
2021-04-28 13:52:18 +0100

    created all status list pages

--- app/routes.js
+++ app/routes.js
@@ -14,7 +14,21 @@ router.post('/finding-and-organising/v1/status-filter', function (req, res) {
     if (status === 'unassigned') {
       res.redirect('/finding-and-organising/v1/unassigned-list')
     } else if (status === 'in-review') {
-        res.redirect('/finding-and-organising/v1/unassigned-list')
+        res.redirect('/finding-and-organising/v1/review-list')
+    } else if (status === 'gathering-information') {
+      res.redirect('/finding-and-organising/v1/information-list')
+    } else if (status === 'awaiting-evidence') {
+      res.redirect('/finding-and-organising/v1/evidence-list')
+    } else if (status === 'paper-based-review') {
+      res.redirect('/finding-and-organising/v1/paperbased-list')
+    } else if (status === 'assessment-booked') {
+      res.redirect('/finding-and-organising/v1/assessment-list')
+    } else if (status === 'awaiting-decision') {
+      res.redirect('/finding-and-organising/v1/decision-list')
+    } else if (status === 'done') {
+      res.redirect('/finding-and-organising/v1/done-list')
+    } else if (status === 'withdrawn') {
+      res.redirect('/finding-and-organising/v1/withdrawn-list')
     } else {
       res.redirect('/finding-and-organising/v1/error')
     }

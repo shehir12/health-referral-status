d8d077a4815bee71ba14e39c80db7ff7098957d8
Leah Smith <leah.smith1@engineering.digital.dwp.gov.uk>
2021-04-28 16:16:40 +0100

    created archive and updated latest prototype

--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,36 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Branching in archive
+router.post('/finding-and-organising/archive/v1/status-filter', function (req, res) {
+  
+  const status = req.session.data['status']
+
+  if (status === 'unassigned') {
+    res.redirect('/finding-and-organising/v1/unassigned-list')
+  } else if (status === 'in-review') {
+      res.redirect('/finding-and-organising/v1/review-list')
+  } else if (status === 'gathering-information') {
+    res.redirect('/finding-and-organising/v1/information-list')
+  } else if (status === 'awaiting-evidence') {
+    res.redirect('/finding-and-organising/v1/evidence-list')
+  } else if (status === 'paper-based-review') {
+    res.redirect('/finding-and-organising/v1/paperbased-list')
+  } else if (status === 'assessment-booked') {
+    res.redirect('/finding-and-organising/v1/assessment-list')
+  } else if (status === 'awaiting-decision') {
+    res.redirect('/finding-and-organising/v1/decision-list')
+  } else if (status === 'done') {
+    res.redirect('/finding-and-organising/v1/done-list')
+  } else if (status === 'withdrawn') {
+    res.redirect('/finding-and-organising/v1/withdrawn-list')
+  } else {
+    res.redirect('/finding-and-organising/v1/error')
+  }
+})
+
 // Branching
 router.post('/finding-and-organising/v1/status-filter', function (req, res) {
-    // Get the answer from session data
-    // The name between the quotes is the same as the 'name' attribute on the input elements
-    // However in JavaScript we can't use hyphens in variable names
   
     const status = req.session.data['status']
   

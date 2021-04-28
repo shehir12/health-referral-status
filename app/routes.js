76a063d570b656e4222974240cede036452a6505
Leah Smith <leah.smith1@engineering.digital.dwp.gov.uk>
2021-04-28 10:57:17 +0100

    fixing journey routes for new find or filter page

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,23 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Branching
+router.post('finding-and-organising/v1/error', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+  
+    const status = req.session.data['status']
+  
+    if (status === 'unassigned') {
+      res.redirect('/views/finding-and-organising/v1/unassigned-list')
+    }
+    if (status === 'in-review') {
+        res.redirect('/views/finding-and-organising/v1/unassigned-list')
+    } else {
+      res.redirect('/views/finding-and-organising/v1/error')
+    }
+  })
+
+
 module.exports = router

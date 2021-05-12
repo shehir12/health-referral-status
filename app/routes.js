8e425461a1354fabb7a8dede8ddaa28c7356533a
Leah Smith <leah.smith1@engineering.digital.dwp.gov.uk>
2021-05-12 15:01:35 +0100

    new version for list view

--- app/routes.js
+++ app/routes.js
@@ -90,4 +90,33 @@ router.post('/finding-and-organising/v1/status-filter', function (req, res) {
   })
 
 
+  router.post('/finding-and-organising/v2/status-filter', function (req, res) {
+  
+    const status = req.session.data['status']
+    req.session.data['status']=''
+  
+    if (status === 'unassigned') {
+      res.redirect('/finding-and-organising/v2/unassigned-list')
+    } else if (status === 'in-review') {
+        res.redirect('/finding-and-organising/v2/review-list')
+    } else if (status === 'gathering-information') {
+      res.redirect('/finding-and-organising/v2/information-list')
+    } else if (status === 'awaiting-evidence') {
+      res.redirect('/finding-and-organising/v2/evidence-list')
+    } else if (status === 'paper-based-review') {
+      res.redirect('/finding-and-organising/v2/paperbased-list')
+    } else if (status === 'assessment-booked') {
+      res.redirect('/finding-and-organising/v2/assessment-list')
+    } else if (status === 'awaiting-decision') {
+      res.redirect('/finding-and-organising/v2/decision-list')
+    } else if (status === 'done') {
+      res.redirect('/finding-and-organising/v2/done-list')
+    } else if (status === 'withdrawn') {
+      res.redirect('/finding-and-organising/v2/withdrawn-list')
+    } else {
+      res.redirect('/finding-and-organising/v2/error')
+    }
+  })
+
+
 module.exports = router

b4fe131f7025efb6ad1342a6a1f766ed2070691c
Leah Smith <leah.smith1@engineering.digital.dwp.gov.uk>
2021-05-18 16:59:50 +0100

    changed v3 to v2 and updated routes

--- app/routes.js
+++ app/routes.js
@@ -117,81 +117,81 @@ router.post('/finding-and-organising/v1/status-filter', function (req, res) {
     }
   })
 
-  //Branching v3
+  //Branching v2
 
-  router.post('/finding-and-organising/v3/status-filter2', function (req, res) {
+  router.post('/finding-and-organising/v2/status-filter2', function (req, res) {
   
     const status2 = req.session.data['status2']
     req.session.data['status2']=''
   
     if (status2 === 'list-1') {
-      res.redirect('/finding-and-organising/v3/list-1')
+      res.redirect('/finding-and-organising/v2/list-1')
     } else if (status2 === 'list-2') {
-        res.redirect('/finding-and-organising/v3/list-2')
+        res.redirect('/finding-and-organising/v2/list-2')
     } else if (status2 === 'list-3') {
-      res.redirect('/finding-and-organising/v3/list-3')
+      res.redirect('/finding-and-organising/v2/list-3')
     } else if (status2 === 'list-4') {
-      res.redirect('/finding-and-organising/v3/list-4')
+      res.redirect('/finding-and-organising/v2/list-4')
     } else if (status2 === 'list-5') {
-      res.redirect('/finding-and-organising/v3/list-5')
+      res.redirect('/finding-and-organising/v2/list-5')
     } else if (status2 === 'list-6') {
-      res.redirect('/finding-and-organising/v3/list-6')
+      res.redirect('/finding-and-organising/v2/list-6')
     } else if (status2 === 'unassigned') {
-      res.redirect('/finding-and-organising/v3/unassigned-list')
+      res.redirect('/finding-and-organising/v2/unassigned-list')
     } else if (status2 === 'in-review') {
-      res.redirect('/finding-and-organising/v3/review-list')
+      res.redirect('/finding-and-organising/v2/review-list')
     } else if (status2 === 'gathering-information') {
-      res.redirect('/finding-and-organising/v3/information-list')
+      res.redirect('/finding-and-organising/v2/information-list')
     } else if (status2 === 'awaiting-evidence') {
-      res.redirect('/finding-and-organising/v3/evidence-list')
+      res.redirect('/finding-and-organising/v2/evidence-list')
     } else if (status2 === 'paper-based-review') {
-      res.redirect('/finding-and-organising/v3/paperbased-list')
+      res.redirect('/finding-and-organising/v2/paperbased-list')
     } else if (status2 === 'assessment-booked') {
-      res.redirect('/finding-and-organising/v3/assessment-list')
+      res.redirect('/finding-and-organising/v2/assessment-list')
     } else if (status2 === 'awaiting-decision') {
-      res.redirect('/finding-and-organising/v3/decision-list')
+      res.redirect('/finding-and-organising/v2/decision-list')
     } else if (status2 === 'done') {
-      res.redirect('/finding-and-organising/v3/done-list')
+      res.redirect('/finding-and-organising/v2/done-list')
     } else if (status2 === 'withdrawn') {
-      res.redirect('/finding-and-organising/v3/withdrawn-list')
+      res.redirect('/finding-and-organising/v2/withdrawn-list')
     } else {
-      res.redirect('/finding-and-organising/v3/error')
+      res.redirect('/finding-and-organising/v2/error')
     }
   })
 
-  router.post('/finding-and-organising/v3/assign-list', function (req, res) {
+  router.post('/finding-and-organising/v2/assign-list', function (req, res) {
   
     const list = req.session.data['list']
     req.session.data['list']=''
   
     if (list === 'list1') {
-      res.redirect('/finding-and-organising/v3/confirmation-list1')
+      res.redirect('/finding-and-organising/v2/confirmation-list1')
     } else if (list === 'list2') {
-      res.redirect('/finding-and-organising/v3/confirmation-list2')
+      res.redirect('/finding-and-organising/v2/confirmation-list2')
     } else if (list === 'list3') {
-      res.redirect('/finding-and-organising/v3/confirmation-list3')
+      res.redirect('/finding-and-organising/v2/confirmation-list3')
     } else if (list === 'list4') {
-      res.redirect('/finding-and-organising/v3/confirmation-list4')
+      res.redirect('/finding-and-organising/v2/confirmation-list4')
     } else if (list === 'list5') {
-      res.redirect('/finding-and-organising/v3/confirmation-list5')
+      res.redirect('/finding-and-organising/v2/confirmation-list5')
     } else if (list === 'list6') {
-      res.redirect('/finding-and-organising/v3/confirmation-list6')
+      res.redirect('/finding-and-organising/v2/confirmation-list6')
     } else if (list === 'new-list') {
-      res.redirect('/finding-and-organising/v3/new-list')
+      res.redirect('/finding-and-organising/v2/new-list')
     } else {
-      res.redirect('/finding-and-organising/v3/error-list')
+      res.redirect('/finding-and-organising/v2/error-list')
     }
   })
 
-  router.post('/finding-and-organising/v3/remove-list', function (req, res) {
+  router.post('/finding-and-organising/v2/remove-list', function (req, res) {
   
     const removeList = req.session.data['remove-list']
     req.session.data['remove-list']=''
   
     if (removeList === 'yes') {
-      res.redirect('/finding-and-organising/v3/confirmation-list-removed')
+      res.redirect('/finding-and-organising/v2/confirmation-list-removed')
     } else {
-      res.redirect('/finding-and-organising/v3/claimant-detail')
+      res.redirect('/finding-and-organising/v2/claimant-detail')
     }
   })
 
